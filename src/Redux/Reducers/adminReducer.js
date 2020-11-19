213137e09a9cf9080484bf3c7b052143cbc2e908
Maciel Galeano <macielgaleano.jh@gmail.com>
2020-11-19 12:12:18 -0300

    Update adminReducer.js

--- src/Redux/Reducers/adminReducer.js
+++ src/Redux/Reducers/adminReducer.js
@@ -1,5 +1,12 @@
+import { REHYDRATE } from "redux-persist";
 function adminReducer(state = {}, action) {
   switch (action.type) {
+    case REHYDRATE:
+      if (state.payload) {
+        return { ...state.payload.admin.token };
+      }
+      return state;
+
     case "REGISTER":
       return { ...action.payload.data };
 
