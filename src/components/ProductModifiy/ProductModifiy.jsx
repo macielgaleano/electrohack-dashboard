4eb60e3e5734d311af0a85bdd03061f49aa77caf
Maciel Galeano <macielgaleano.jh@gmail.com>
2020-11-20 11:40:41 -0300

    Update ProductModifiy.jsx

--- src/components/ProductModifiy/ProductModifiy.jsx
+++ src/components/ProductModifiy/ProductModifiy.jsx
@@ -1,15 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {
-  Grid,
-  Row,
-  Col,
-  Table,
-  FormGroup,
-  FormControl,
-  Button,
-} from "react-bootstrap";
+import { Grid, Row, Col, Table, FormGroup, FormControl, Button } from "react-bootstrap";
 import { useSelector } from "react-redux";
 
 export default function ProductModifiy() {
@@ -17,64 +9,12 @@ export default function ProductModifiy() {
   const [products, setProducts] = useState(null);
   const [categories, setCategories] = useState(null);
 
-<<<<<<< Updated upstream
-  function deleteItem(slug, token) {
-    axios
-      .delete("http://localhost:8000/api/admin/productos", {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-        data: { slug: slug },
-      })
-      .then((res) => {
-        const newsProducts = products.filter((product) => product.slug !== slug);
-        setProducts(newsProducts);
-      });
-  }
-
-  function updateProduct(e, formData, token) {
-    setShow(false);
-    axios.put("https://electrohack-server.vercel.app/productos", {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-      data: {
-        name: formData.name,
-        price: formData.price,
-        outstanding: formData.outstanding,
-        description: formData.description,
-      },
-    });
-  }
-
-=======
->>>>>>> Stashed changes
   useEffect(() => {
     axios.get("https://electrohack-server.vercel.app/productos").then((res) => {
       setProducts(res.data);
     });
   }, []);
 
-<<<<<<< Updated upstream
-  return (
-    <Grid>
-      {products &&
-        products.map((product) => {
-          return (
-            <div className="item-box">
-              <Row key={product.slug}>
-                <Col md={4}>{product.name}</Col>
-                <Col md={2}>$ {product.price}</Col>
-                <Col md={2}>
-                  <Button bsStyle="success">Cambiar Imagen</Button>
-                </Col>
-                <Col md={2}>
-                  <Button bsStyle="primary" onClick={handleShow}>
-                    Editar
-                  </Button>
-=======
   useEffect(() => {
     axios
       .get("https://electrohack-server.vercel.app/productos/lista/categorias")
@@ -86,7 +26,6 @@ export default function ProductModifiy() {
   function handleSubmit(data) {
     axios.post("https://electrohack-server.vercel.app/api/admin/productos");
   }
->>>>>>> Stashed changes
 
   return (
     <>
@@ -125,16 +64,11 @@ export default function ProductModifiy() {
                         </td>
                         <td>
                           <FormGroup controlId="formControlsSelect">
-                            <FormControl
-                              componentClass="select"
-                              name="category"
-                            >
+                            <FormControl componentClass="select" name="category">
                               {categories &&
                                 categories.map((category) => {
                                   return (
-                                    <option value={category._id}>
-                                      {category.name}
-                                    </option>
+                                    <option value={category._id}>{category.name}</option>
                                   );
                                 })}
                             </FormControl>
@@ -165,10 +99,7 @@ export default function ProductModifiy() {
                         <td>
                           <div class="input-group mb-3">
                             <div class="input-group-prepend">
-                              <span
-                                class="input-group-text"
-                                id="inputGroupFileAddon01"
-                              >
+                              <span class="input-group-text" id="inputGroupFileAddon01">
                                 Upload
                               </span>
                             </div>
@@ -179,10 +110,7 @@ export default function ProductModifiy() {
                                 id="inputGroupFile01"
                                 aria-describedby="inputGroupFileAddon01"
                               />
-                              <label
-                                class="custom-file-label"
-                                for="inputGroupFile01"
-                              >
+                              <label class="custom-file-label" for="inputGroupFile01">
                                 Choose file
                               </label>
                             </div>
@@ -211,59 +139,6 @@ export default function ProductModifiy() {
                             onClick={handleSubmit}
                             className="btn btn-small btn-primary"
                           >
-<<<<<<< Updated upstream
-                            {product.outstanding ? (
-                              <>
-                                <option value={false}>No</option>
-                                <option value={true} selected>
-                                  Si
-                                </option>
-                              </>
-                            ) : (
-                              <>
-                                <option value={false} selected>
-                                  No
-                                </option>
-                                <option value={true}>Si</option>
-                              </>
-                            )}
-                          </select>
-                        </div>
-                      </Form>
-                    </Modal.Body>
-                    <Modal.Footer>
-                      <Button variant="secondary" onClick={handleClose}>
-                        Cerrar
-                      </Button>
-                      <Button
-                        onSubmit={(e) => {
-                          updateProduct(
-                            e,
-                            { name, description, price, outstanding },
-                            token
-                          );
-                        }}
-                        variant="primary"
-                      >
-                        Actualizar
-                      </Button>
-                    </Modal.Footer>
-                  </Modal>
-                </Col>
-                <Col md={2}>
-                  <Button
-                    bsStyle="danger"
-                    onClick={() => deleteItem(product.slug, token)}
-                  >
-                    Eliminar
-                  </Button>
-                </Col>
-              </Row>
-            </div>
-          );
-        })}
-    </Grid>
-=======
                             Editar
                           </Button>
                         </td>
@@ -277,6 +152,5 @@ export default function ProductModifiy() {
         </Row>
       </Grid>
     </>
->>>>>>> Stashed changes
   );
 }
