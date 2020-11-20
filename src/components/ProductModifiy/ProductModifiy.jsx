d9c46ce9db4cd6805592f7abfc62053369704b4a
SebitaCR14 <55540031+SebitaCR14@users.noreply.github.com>
2020-11-20 01:10:52 -0300

    Update ProductModifiy.jsx

--- src/components/ProductModifiy/ProductModifiy.jsx
+++ src/components/ProductModifiy/ProductModifiy.jsx
@@ -1,12 +1,20 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Grid, Row, Col, Button } from "react-bootstrap";
+import { Grid, Row, Col, Button, Modal, Form } from "react-bootstrap";
 import { useSelector } from "react-redux";
+import { isForInStatement, isForOfStatement } from "typescript";
 
 const ProductModify = () => {
   const token = useSelector((state) => state.admin.token);
   const [products, setProducts] = useState(null);
+  const [name, setName] = useState();
+  const [description, setDescription] = useState();
+  const [price, setPrice] = useState();
+  const [outstanding, setOutstanding] = useState();
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   function deleteItem(slug, token) {
     axios
@@ -23,6 +31,25 @@ const ProductModify = () => {
       });
   }
 
+<<<<<<< Updated upstream
+=======
+  function updateProduct(e, formData, token) {
+    setShow(false);
+    axios.put("https://electrohack-server.vercel.app/productos", {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      data: {
+        name: formData.name,
+        price: formData.price,
+        outstanding: formData.outstanding,
+        description: formData.description,
+      },
+    });
+  }
+
+>>>>>>> Stashed changes
   useEffect(() => {
     axios.get("https://electrohack-server.vercel.app/productos").then((res) => {
       setProducts(res.data);
@@ -34,6 +61,7 @@ const ProductModify = () => {
       {products &&
         products.map((product) => {
           return (
+<<<<<<< Updated upstream
             <Row key={product.slug}>
               <Col md={5}>{product.name}</Col>
               <Col md={3}>$ {product.price}</Col>
@@ -46,6 +74,126 @@ const ProductModify = () => {
                 </Button>
               </Col>
             </Row>
+=======
+            <div className="item-box">
+              <Row key={product.slug}>
+                <Col md={4}>{product.name}</Col>
+                <Col md={2}>$ {product.price}</Col>
+                <Col md={2}>
+                  <Button bsStyle="success">Cambiar Imagen</Button>
+                </Col>
+                <Col md={2}>
+                  <Button bsStyle="primary" onClick={handleShow}>
+                    Editar
+                  </Button>
+
+                  <Modal
+                    show={show}
+                    onHide={handleClose}
+                    backdrop="static"
+                    keyboard={false}
+                  >
+                    <Modal.Header closeButton>
+                      <Modal.Title>Modificar datos del producto</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                      <Form>
+                        <div class="form-group">
+                          <label for="name">Nombre del Producto</label>
+                          <input
+                            type="text"
+                            name="name"
+                            onChange={(e) => {
+                              setName(e.target.value);
+                            }}
+                            value={product.name}
+                            class="form-control"
+                            id="name"
+                          />
+                        </div>
+                        <div class="form-group">
+                          <label for="price">Precio</label>
+                          <input
+                            type="text"
+                            name="price"
+                            onChange={(e) => {
+                              setPrice(e.target.value);
+                            }}
+                            value={product.price}
+                            class="form-control"
+                            id="price"
+                          />
+                        </div>
+                        <div class="form-group">
+                          <label for="description">Descripción</label>
+                          <textarea
+                            type="text"
+                            name="description"
+                            onChange={(e) => {
+                              setDescription(e.target.value);
+                            }}
+                            value={product.description}
+                            class="form-control"
+                            id="description"
+                            placeholder="Example input placeholder"
+                          />
+                        </div>
+                        <div class="form-group col-md-4">
+                          <label for="outstanding">Destacado</label>
+                          <select
+                            id="outstanding"
+                            name="outstanding"
+                            class="form-control"
+                          >
+                            {product.outstanding ? (
+                              <>
+                                <option value={false}>No</option>
+                                <option value={true} selected>
+                                  Si
+                                </option>
+                              </>
+                            ) : (
+                              <>
+                                <option value={false} selected>
+                                  No
+                                </option>
+                                <option value={true}>Si</option>
+                              </>
+                            )}
+                          </select>
+                        </div>
+                      </Form>
+                    </Modal.Body>
+                    <Modal.Footer>
+                      <Button variant="secondary" onClick={handleClose}>
+                        Cerrar
+                      </Button>
+                      <Button
+                        onSubmit={(e) => {
+                          updateProduct(
+                            e,
+                            { name, description, price, outstanding },
+                            token
+                          );
+                        }}
+                        variant="primary"
+                      >
+                        Actualizar
+                      </Button>
+                    </Modal.Footer>
+                  </Modal>
+                </Col>
+                <Col md={2}>
+                  <Button
+                    bsStyle="danger"
+                    onClick={() => deleteItem(product.slug, token)}
+                  >
+                    Eliminar
+                  </Button>
+                </Col>
+              </Row>
+            </div>
+>>>>>>> Stashed changes
           );
         })}
     </Grid>
