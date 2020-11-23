961a52d0db35b2b1792bb32b67f243f947cd4a6d
SebitaCR14 <55540031+SebitaCR14@users.noreply.github.com>
2020-11-23 15:22:31 -0300

    Update Category.jsx

---
+++ src/components/Category/Category.jsx
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import {
+  Grid,
+  Row,
+  Col,
+  Button,
+  FormGroup,
+  InputGroup,
+  FormControl,
+} from "react-bootstrap";
+import { useSelector } from "react-redux";
+
+export default function Category() {
+  const token = useSelector((state) => state.admin.token);
+  const [categories, setCategories] = useState(null);
+  const [newCategoryName, setNewCategoryName] = useState("");
+
+  function updateCategory(newCategoryName, categoryName) {
+    axios.put(
+      "https://electrohack-server.vercel.app/api/admin/categorias",
+      {
+        nameToSearch: categoryName,
+        newCategoryName: newCategoryName,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+  }
+
+  useEffect(() => {
+    axios
+      .get("https://electrohack-server.vercel.app/productos/lista/categorias")
+      .then((res) => {
+        setCategories(res.data);
+      });
+  }, []);
+  return (
+    <>
+      {console.log(categories)}
+      <h1>Categorias</h1>
+      <Grid>
+        {categories &&
+          categories.map((category) => {
+            return (
+              <Row>
+                <Col md={6}>
+                  <h6>{category.name}</h6>
+                  <h6>{category.createdAt}</h6>
+                </Col>
+                <Col md={6}>
+                  <FormGroup>
+                    <InputGroup>
+                      <InputGroup.Button>
+                        <Button
+                          onClick={() =>
+                            updateCategory(newCategoryName, category.name)
+                          }
+                        >
+                          Cambiar Nombre
+                        </Button>
+                      </InputGroup.Button>
+                      <FormControl
+                        type="text"
+                        placeholder="Introduce un nuevo nombre para la categoría..."
+                        onChange={(e) => {
+                          setNewCategoryName(e.target.value);
+                        }}
+                      />
+                    </InputGroup>
+                  </FormGroup>
+                </Col>
+              </Row>
+            );
+          })}
+      </Grid>
+    </>
+  );
+}
